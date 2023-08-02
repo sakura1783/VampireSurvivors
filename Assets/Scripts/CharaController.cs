f139e78a90e589ca21d88de53340a0e3a0a6b614
sakura1783 <sarukaniumai@gmail.com>
2023-08-02 12:06:17 +0900

    移動処理　完了

---
+++ Assets/Scripts/CharaController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 tapPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Move(tapPos);
+        }
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move(Vector2 tapPos)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, tapPos, moveSpeed * Time.deltaTime);
+    }
+}
