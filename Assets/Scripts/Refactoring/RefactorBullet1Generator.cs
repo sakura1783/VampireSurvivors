4b46cea960886e0c606f73d442cc6d97b1e68fc8
sakura1783 <sarukaniumai@gmail.com>
2023-10-06 09:08:04 +0900

    オブジェクトプールを利用した弾生成の最適化② 完了

---
+++ Assets/Scripts/Refactoring/RefactorBullet1Generator.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// オブジェクトプールとクラス継承を利用
+/// </summary>
+public class RefactorBullet1Generator : BulletGeneratorBase
+{
+    public int bulletLevel = 1;
+
+    private List<BulletBase> bulletList = new();
+
+
+    /// <summary>
+    /// オブジェクトプールと継承を使った弾の生成
+    /// </summary>
+    /// <param name="generatePos"></param>
+    public override void GenerateBullet(Vector2 direction)
+    {
+        //プールに弾を全て戻し、リストからも削除
+        DestroyBullets();
+
+        //弾の数に基づいて、各弾の発射角度を計算
+        float offsetDegrees = 360 / bulletLevel;
+
+        //弾のレベルに応じて生成する数を変更
+        for (int i = 0; i < bulletLevel;)
+        {
+            //回転情報にdirectionをかけることで、ここで弾の方向のベクトルが決まる
+            Vector2 offsetDirection = Quaternion.Euler(0, 0, i * offsetDegrees) * direction;
+
+            //オブジェクトの位置から一定距離離れた位置が計算される
+            Vector2 offsetPos = (Vector2)transform.position + offsetDirection;
+
+            //オブジェクトプールから取り出す。なければ新しく生成する
+            BulletBase bullet = GetBullet(offsetPos, Quaternion.identity);
+
+            bullet.transform.SetParent(charaController.transform);
+
+            //弾の初期設定
+            bullet.SetUpBullet(charaController);
+
+            bulletList.Add(bullet);
+        }
+    }
+
+    public override void GenerateBullet<T>(Vector2 direction, T t) { }
+
+    /// <summary>
+    /// BulletGenerator1のDestroyObjectsFromListメソッドををオブジェクトプールを使って記述
+    /// </summary>
+    private void DestroyBullets()
+    {
+        foreach (var bullet in bulletList)
+        {
+            //破壊ではなくプールに戻す
+            bullet.ReleaseBullet();
+        }
+
+        bulletList.Clear();
+    }
+}
